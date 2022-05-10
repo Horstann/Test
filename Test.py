bb3904b1fc96c0fadec620de297a621b15270f42
Horstann <horstannho@gmail.com>
2022-05-10 14:20:02 +0800

    Added a lil' OOP

--- Test.py
+++ Test.py
@@ -1,5 +1,20 @@
-msg = 'h'
-print(msg.upper())
-x = 3
-y = 5
-print(type(x-y))
+class complexNo:
+    def __init__(self, real=0, img=0):
+        self.real = real
+        self.img = img
+    def displayComplex(self):
+        print(f"Your complex number = {self.real} + {self.img}i")
+
+x = y = 0
+while (True):
+    try: x = int(input("Enter an integer: "))
+    except: print("Invalid. Try again")
+    else: break
+print("")
+while (True):
+    try: y = int(input("Enter another integer: "))
+    except: print("Invalid. Try again")
+    else: break
+
+n = complexNo(x, y)
+n.displayComplex()
